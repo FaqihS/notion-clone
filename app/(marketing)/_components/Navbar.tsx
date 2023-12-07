47dc8a42f92cd0b52d8b000ea3f6c84e1a28d255
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-12-07 18:29:26 +0800

    add convex & clerk, spinner components

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,27 +1,44 @@
-'use client'
+"use client";
 
-import useScrollTop from "@/hooks/useScrollTop"
+import useScrollTop from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
 import { ModeToggle } from "@/components/modeToggle";
 import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Spinner } from "@/components/provider/spinner";
 
-export default function Navbar(){
+export default function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const isScrolled = useScrollTop();
 
-
-  return(
-  <div className={cn(
-      "z-50 fixed bg-background dark:bg-[#1f1f1f] top-0 w-full  flex items-center p-4",
-      isScrolled && "border-b shadow-sm "
-    )}>
+  return (
+    <div
+      className={cn(
+        "z-50 fixed bg-background dark:bg-[#1f1f1f] top-0 w-full  flex items-center p-4",
+        isScrolled && "border-b shadow-sm "
+      )}
+    >
       <Logo />
       <div className="flex justify-between w-full items-center gap-x-2 md:ml-auto md:justify-end">
+        {isLoading && <Spinner/>}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button  size="sm">
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
-        <Button>
-          Login
-        </Button>
       </div>
     </div>
-  )
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/themeProvider";
+import ConvexClientProvider from "@/components/provider/convexProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,22 +31,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const className =inter.className + " dark:animate-fade-in-light animate-fade-in"
-  console.log(className)
+  const className =
+    inter.className + " dark:animate-fade-in-light animate-fade-in";
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={className}>
-        {/* <div className=""> */}
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          // disableTransitionOnChange
-          storageKey="jotion-theme"
-        >
-          {children}
-        </ThemeProvider>
-        {/* </div> */}
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            // disableTransitionOnChange
+            storageKey="jotion-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
