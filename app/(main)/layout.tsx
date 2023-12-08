b9ca9e2f29eba0f7072b4882b090e32208981c23
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-12-08 09:05:53 +0800

    add sidebar in /documents layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,12 +3,10 @@ import { Spinner } from "@/components/provider/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation"
 import { ReactNode } from "react";
+import Navigation from "./_components/Navigation";
 
 export default function MainLayout({children}:{children:ReactNode}){
   const {isAuthenticated,isLoading} = useConvexAuth()
-  if(!isAuthenticated){
-    return redirect('/')
-  }
 
   if(isLoading){
     return (
@@ -17,11 +15,15 @@ export default function MainLayout({children}:{children:ReactNode}){
       </div>
     )
   }
+  if(!isAuthenticated){
+    return redirect('/')
+  }
 
 
 
   return (
   <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation/>
       <main className="flex-1 h-full overflow-y-auto">
         {children}
       </main>
